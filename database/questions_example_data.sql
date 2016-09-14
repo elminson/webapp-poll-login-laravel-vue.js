c74ad02961e9261b0ccdfbf2be83336a35118cb7
Elminson De Oleo Baez <edeoleo@gmail.com>
2016-09-14 16:22:13 -0400

    Create questions_example_data.sql

---
+++ database/questions_example_data.sql
@@ -0,0 +1,7 @@
+
+-- ----------------------------
+-- Example Data of questions
+-- ----------------------------
+INSERT INTO `questions` VALUES ('1', 'How did you feel?', 'radio', null, null);
+INSERT INTO `questions` VALUES ('2', 'What did you have for Breakfast?', 'checkbox', null, null);
+INSERT INTO `questions` VALUES ('3', 'Did you run Today ?', 'radio', null, null);
