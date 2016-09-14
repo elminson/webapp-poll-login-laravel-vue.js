5442809f4b94fd99b72b4b4a078df91bcbcfdea0
Elminson De Oleo Baez <edeoleo@gmail.com>
2016-09-14 16:21:53 -0400

    Create answers_example_data.sql

---
+++ database/answers_example_data.sql
@@ -0,0 +1,11 @@
+-- ----------------------------
+-- Example Data of answers
+-- ----------------------------
+INSERT INTO `answers` VALUES ('1', '1', 'Bad', null, null);
+INSERT INTO `answers` VALUES ('2', '1', 'Good', null, null);
+INSERT INTO `answers` VALUES ('3', '1', 'Excelent', null, null);
+INSERT INTO `answers` VALUES ('4', '2', 'Fruits', null, null);
+INSERT INTO `answers` VALUES ('5', '2', 'Cereal', null, null);
+INSERT INTO `answers` VALUES ('6', '2', 'Coffee', null, null);
+INSERT INTO `answers` VALUES ('7', '3', 'Yes', null, null);
+INSERT INTO `answers` VALUES ('8', '3', 'No', null, null);
